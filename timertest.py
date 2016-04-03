a4c025b33ed4a964ebe55abcccb2ac2fde50f3a8
taplo <taplo@qq.com>
2016-04-03 23:49:26 +0800

    20160403
    New Update

---
+++ timertest.py
@@ -0,0 +1,23 @@
+﻿#C:\Users\WangTao\Documents\Python
+#FileName:timertest.py
+# -*- coding:utf-8-*-
+
+#计时测试
+import time
+from timeit import timeit,repeat
+
+
+start_cpu=time.clock()
+start_real=time.time()
+for i in range(500000):
+    a=i**5
+end_cpu=time.clock()
+end_real=time.time()
+print "%f real Seconds"%(end_real-start_real)
+print "%f CPU Seconds"%(end_cpu-start_cpu)
+
+
+t1=timeit('a=9*9*9*9*9*9')
+print t1
+t2=repeat('a=9*9*9*9*9*9')
+print t2
