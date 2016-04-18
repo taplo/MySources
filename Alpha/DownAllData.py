f95bff91daa2d42c8f50968bf135d9b062df49c7
taplo <taplo@qq.com>
2016-04-18 23:54:33 +0800

    20160418 Home
    日常更新：使用多线程的线程池模式进行全部数据的下载并保存到本地的HDF5文件。

---
+++ Alpha/DownAllData.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 18 21:41:36 2016
+
+@author: WangTao
+下载全部日线数据并保存到本地文件
+"""
+import tushare as ts
+import pandas as pd
+from multiprocessing.pool import ThreadPool
+import multiprocessing
+
+#将上市时间的整形变量转换成符合要求的字符串变量
+def ChangeDate(i):
+	if i>0:
+		s=str(i)
+		r=s[0:4]+'-'+s[4:6]+'-'+s[6:8]
+		return r
+	else:
+		return ''
+
+#下载个股全部前复权日线数据
+def DownloadQfqAll(code,st):
+	'''
+	code is stockcode in string type
+	st is time to market in string 'YYYY-MM-DD' type
+	'''
+	if len(st)>2:
+		df=ts.get_h_data(code,start=st)
+		df=df.sort_index(ascending=1)
+	else:
+		df=ts.get_h_data(code)
+	print code+':'+st+' finished!'
+	return [code,df]
+
+
+
+
+
+if __name__ == '__main__':
+
+	FileName='c:\\tmp\\save.h5'
+	#获得网络股票数据列表
+	nbi=ts.get_stock_basics()
+	nbi=nbi.sort_index(ascending=1)
+	lst=nbi.timeToMarket
+	lst=lst.apply(ChangeDate)
+
+	#创建线程池
+	count=multiprocessing.cpu_count()
+	pool=ThreadPool(processes=count*10)
+
+	#启动线程池
+	result=[]
+	date={}
+	for i in xrange(len(lst)):
+	    result.append(pool.apply_async(DownloadQfqAll, (lst.index[i],lst[i])))
+	pool.close()
+	pool.join()
+	print "Sub-threads done."
+	for res in result:
+		t=res.get()
+		date[t[0]]=t[1]
+
+	data=pd.Series(date)
+	data.sort_index(ascending=1)
+	date=data.to_dict()
+	pan=pd.Panel(date)
+	store=pd.HDFStore(FileName,mode='a')
+	store['qfqdata']=pan
+	store.close()
+
+
+	print 'done!'
