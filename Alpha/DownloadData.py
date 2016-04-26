b77dc7a24b29683a65b5e71dfc019857e2c1b268
taplo <taplo@qq.com>
2016-04-26 23:03:31 +0800

    20160426 home -B
    日常更新：下载更新数据程序修正；多线程测试程序——可做模板。

--- Alpha/DownloadData.py
+++ Alpha/DownloadData.py
@@ -14,10 +14,11 @@ import multiprocessing
 import datetime as dt
 import time
 store=object
+totalStatus={}
 
 #将上市时间的整形变量转换成符合要求的字符串变量
 def ChangeDate(i):
-	r=''	
+	r=''
 	if i>0:
 		s=str(i)
 		r=s[0:4]+'-'+s[4:6]+'-'+s[6:8]
@@ -38,14 +39,14 @@ def LoadLocalData(kind):
 		store=pd.HDFStore(u'D:\\用户目录\\My Documents\\Python\\save.h5',mode='a')
 	else:
 		store=pd.HDFStore('d:\\save.h5',mode='a')
-	
+
 	try:
 		pan=store[kind]
 	except:
 		pan=pd.Panel()
-	
+
 	return pan
-	
+
 #保存本地数据
 def SaveLocalData(kind,pan):
 	global store
@@ -90,47 +91,68 @@ def UpdateDate(s):
 	while ts.is_holiis_holiday(str(wd)):
 		wd = wd-dt.timedelta(days=1)
 
-	#有初始时间的设置为5日前，没有的设为3日前
+	#有初始时间的设置为10日前，没有的设为5日前
 	if len(s)>1:
-		day=str(wd-dt.timedelta(days=5))
+		day=str(wd-dt.timedelta(days=10))
 	else:
-		day=str(wd-dt.timedelta(days=3))
-	
+		day=str(wd-dt.timedelta(days=5))
+
 	return day
-		
+
 #检查所有线程是否全部完成
 def CheckResult(result):
+	u'检查线程是否全部完成'
 	if type(result)==list and type(result[0])==multiprocessing.pool.AsyncResult:
 		lst=[]
+		lst2=[]
+		status={}
 		for res in result:
-			lst.append(res.ready())
-			return sum(lst) 
+			t=res.ready()
+			lst.append(t)
+			if t:
+				try:
+					lst2.append(res.successful())
+				except:
+					lst2.append(False)
+
+		status['all']=len(result)
+		status['finished']=sum(lst)
+		status['successful']=sum(lst2)
 	else:
-		return 0
-	
+		status['all']=len(result)
+		status['finished']=len(result)
+		status['successful']=0
+
+	return status
+
 
 #按照列表多线程下载数据的函数
 def MultiDownload(kind,lst):
 	#创建线程池
 	count=multiprocessing.cpu_count()
-	pool=ThreadPool(processes=count*2)
+	pool=ThreadPool(processes=count)
 
 	#启动线程池
 	result=[]
 	date={}
-	for i in xrange(len(lst)):
-		if kind=='qfq':
+	if kind=='qfq':
+		for i in xrange(len(lst)):
 		    result.append(pool.apply_async(DownloadQfqAll, (lst.index[i],lst[i])))
-		else:
+	else:
+		for i in xrange(len(lst)):
 			result.append(pool.apply_async(DownloadCqAll, (lst.index[i],lst[i])))
 	pool.close()
-	qcount=CheckResult(result)
-	while qcount:
-		print u'\n已经完成%s/%s项下载任务！'%(qcount,len(result))
-		time.sleep(15)
-		qcount=CheckResult(result)
+	status=CheckResult(result)
+	oldq=0
+	while status['finished']<status['all']:
+		if status['finished']!=oldq:
+			print u'\n\t\t已经完成%s/%s项下载任务！其中成功%s个！'%(status['finished'],status['all'],status['successful'])
+		time.sleep(0.1)
+		oldq=status['finished']
+		status=CheckResult(result)
 	pool.join()
-	print "多线程下载结束！"
+	#print "多线程下载结束！"
+	totalStatus['Download']=u'已经完成%s/%s项下载任务！其中成功%s个！'%(status['finished'],status['all'],status['successful'])
 	for res in result:
 		try:
 			t=res.get()
@@ -142,7 +164,7 @@ def MultiDownload(kind,lst):
 	pan=pan.sort_index(axis=0,ascending=1)
 
 	return pan
-	
+
 #检查个股日线数据的情况并更新的单线程函数
 def UpdateStockData(kind,code,df):
 	u'更新个股最新数据并对清权股票数据更新。如果无需更新则返回原来的df！'
@@ -152,10 +174,10 @@ def UpdateStockData(kind,code,df):
 		wd = wd-dt.timedelta(days=1)
 	lastday=pd.Timestamp(wd)
 	#对比日期
-	df = df.dropna()	
+	df = df.dropna()
 	df = df.sort_index(ascending=1)
 	check = df.iloc[-1]
-	
+
 	if lastday>check.name:
 		if kind=='qfq':
 			df1 = DownloadQfqAll(code,UpdateDate(str(wd)))
@@ -163,8 +185,8 @@ def UpdateStockData(kind,code,df):
 			df1 = DownloadCqAll(code,UpdateDate(str(wd)))
 		else:
 			df1=pd.DataFrame()
-		
-		if len(df1)>0	:	
+
+		if len(df1)>0	:
 			#对比是否发生清权
 			dp=pd.concat([df,df1])
 			dp=dp.drop_duplicates()
@@ -185,7 +207,7 @@ def UpdateStockData(kind,code,df):
 		#无更新值
 		return df
 
-	
+
 #按数据表多线程更新股票数据的多线程启动函数
 def MultiUpdate(kind,pan):
 	u'多线程更新股票日线数据'
@@ -198,17 +220,21 @@ def MultiUpdate(kind,pan):
 	qfq=['qfq']*len(pan)
 	cq=['cq']*len(pan)
 	for i in xrange(len(pan)):
-		if kind=='qfq':	    
+		if kind=='qfq':
 			result.append(pool.apply_async(UpdateStockData, (qfq,pan.items[i],pan[pan.items[i]])))
 		elif kind=='cq':
 			result.append(pool.apply_async(UpdateStockData, (cq,pan.items[i],pan[pan.items[i]])))
 	pool.close()
-	qcount=CheckResult(result)
-	while qcount:
-		print u'\n已经完成%s/%s项更新任务！'%(qcount,len(result))
-		time.sleep(15)
-		qcount=CheckResult(result)
+	status=CheckResult(result)
+	oldq=0
+	while status['finished']<status['all']:
+		if status['finished']!=oldq:
+			print u'\n\t\t已经完成%s/%s项更新任务！其中成功%s个！'%(status['finished'],status['all'],status['successful'])
+		time.sleep(0.1)
+		oldq=status['finished']
+		status=CheckResult(result)
 	pool.join()
+	totalStatus['Update']=u'已经完成%s/%s项更新任务！其中成功%s个！'%(status['finished'],status['all'],status['successful'])
 	for res in result:
 		try:
 			t=res.get()
@@ -220,16 +246,17 @@ def MultiUpdate(kind,pan):
 	date=data.to_dict()
 	pan=pd.Panel(date)
 	pan=pan.sort_index()
-	
+
 	return pan
 
 
 if __name__ == '__main__':
-	
-	
-	global store	
-	
-	
+
+	totalStatus['Starttime']=dt.datetime.now()
+
+	global store
+
+
 	#获得股票列表
 	bi=ts.get_stock_basics()
 	bi=bi.sort_index(ascending=1)
@@ -239,44 +266,49 @@ if __name__ == '__main__':
 	#-----------------------前复权数据处理--------------------------------------
 	#从本地加载数据
 	pan=LoadLocalData('qfq')#加载前复权数据
-	
+
 	#补全缺失的个股
 	#获得缺失的股票列表
 	miss=lst.drop(pan.items.tolist())
 	#下载缺失股票数据
-	if len(miss)>0:	
+	if len(miss)>0:
 		misspan=MultiDownload('qfq',miss)
 		#合并缺失数据
 		pan=pd.concat([pan,misspan],axis=0)
 		pan=pan.sort_index(axis=0,ascending=1)
 	#更新日线数据
 	pan=MultiUpdate('qfq',pan)
-		
+
 	#保存数据文件
 	SaveLocalData('qfq',pan)
-	
+
 	#------------------------------除权数据处理---------------------------------
 	#从本地加载数据
-	pan=LoadLocalData('cq')#加载前复权数据
-	
+	pan=LoadLocalData('cq')#加载除权数据
+
 	#补全缺失的个股
 	#获得缺失的股票列表
 	miss=lst.drop(pan.items.tolist())
 	#下载缺失股票数据
-	if len(miss)>0:	
+	if len(miss)>0:
 		misspan=MultiDownload('cq',miss)
 		#合并缺失数据
 		pan=pd.concat([pan,misspan],axis=0)
 		pan=pan.sort_index(axis=0,ascending=1)
 	#更新日线数据
 	pan=MultiUpdate('cq',pan)
-	
+
 	#保存数据文件
 	SaveLocalData('cq',pan)
 
-	#---------------------------数据处理结束-------------------------------------	
-	
-	
-	
+	#---------------------------数据处理结束-------------------------------------
+	totalStatus['Endtime']=dt.datetime.now()
+
+	print u'下载情况：',totalStatus['Download']
+	print u'更新情况：',totalStatus['Update']
+	print u'开始时间：',totalStatus['Starttime']
+	print u'结束时间：',totalStatus['Endtime']
+	print u'用时:',totalStatus['Endtime']-totalStatus['Starttime']
+
 
 
--- Alpha/MultiThreadTest.py
+++ Alpha/MultiThreadTest.py
@@ -10,33 +10,69 @@ Created on Mon Apr 25 21:54:20 2016
 from multiprocessing.pool import ThreadPool
 import multiprocessing
 import time
-lst=[0]*10 #公用的数据，安全起见，每个线程只操作自己的那个位置。
+status={}
+n = 20
+lst = [0]*n #公用的数据，安全起见，每个线程只操作自己的那个位置。
 
 def func(l):
-	start=time.clock()
+	start = time.clock()
 	global lst
-	count=0
+	count = 0
 	for i in xrange(l):
 		for j in xrange(i):
-			count+=1
-	end=time.clock()
-	s=[str(l),str(end-start)]
-	lst[l]=end-start
+			count += 1
+	end = time.clock()
+	s = [str(l), str(end-start)]
+	lst[l] = end-start
 	return s
+
+#检查所有线程是否全部完成
+def CheckResult(result):
+	u'检查线程是否全部完成'
+	global status
+	if type(result) == list and type(result[0]) == multiprocessing.pool.AsyncResult:
+		lst = []
+		lst2 = []
+		status = {}
+		for res in result:
+			t = res.ready()
+			lst.append(t)
+			if t:
+				try:
+					lst2.append(res.successful())
+				except:
+					lst2.append(False)
+		status['all'] = len(result)
+		status['finished'] = sum(lst)
+		status['successful'] = sum(lst2)
+	else:
+		status['all'] = len(result)
+		status['finished'] = len(result)
+		status['successful'] = 0
+
+	return status
 
 
 if __name__ == "__main__":
+	global status
 	#创建线程池
-	count=multiprocessing.cpu_count()
-	pool=ThreadPool(processes=count)
+	count = multiprocessing.cpu_count()
+	pool = ThreadPool(processes=count)
 
-	result=[]
-	for s in xrange(10):
-	    result.append(pool.apply_async(func, (s, )))
+	result = []
+	for s in xrange(n):
+		result.append(pool.apply_async(func, (s, )))
 	pool.close()
+	status = CheckResult(result)
+	oldq = 0
+	while status['finished'] < status['all']:
+		if status['finished'] != oldq:
+			print u'\n已经完成%s/%s项下载任务！其中成功%s个！'%(status['finished'], status['all'], status['successful'])
+		time.sleep(0.1)
+		oldq = status['finished']
+		status = CheckResult(result)
 	pool.join()
 	print "Sub-process(es) done."
 	for res in result:
-		t=res.get()
-		print "No.%s processing used %s seconds!"%(t[0],t[1])
-	print lst
+		t = res.get()
+		print "No.%s processing used %s seconds!"%(t[0], t[1])
